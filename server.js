b39caa73d4091e9a6bc038dae97c40b3a48bbe6e
rghimire <rghimire@cs.odu.edu>
2020-03-17 20:24:38 -0400

    Added a postgresql to the server on heroku

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const PORT = process.env.PORT
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'face-recognition'
+      connectString : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
